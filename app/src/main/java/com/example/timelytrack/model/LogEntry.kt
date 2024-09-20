f0d839506924bc8d5db7e86483a8fcddac17986e
Neel <send2neelpatel@gmail.com>
2024-09-20 10:28:01 -0400

    Planning for future expansion of data

--- app/src/main/java/com/example/timelytrack/model/LogEntry.kt
+++ app/src/main/java/com/example/timelytrack/model/LogEntry.kt
@@ -2,7 +2,8 @@ package com.example.timelytrack.model
 
 data class LogEntry(
     val startTimestamp: Long,
-    var endTimestamp: Long ?= null // nullable for incomplete logs
-//    var logCategorization: String
+    var endTimestamp: Long ?= null, // nullable for incomplete logs
+//     var logCategory: String, // Which tracker is it part of
+//     var activity: String // Which activity is it part of
 
 )
--- app/src/main/java/com/example/timelytrack/viewmodel/LogViewModel.kt
+++ app/src/main/java/com/example/timelytrack/viewmodel/LogViewModel.kt
@@ -8,10 +8,13 @@ class LogViewModel : ViewModel() {
     var logEntries = mutableStateListOf<LogEntry>()
         private set
 
+
+    //=== Adds
     fun addLogEntry() {
         logEntries.add(LogEntry(startTimestamp = System.currentTimeMillis()))
     }
 
+    // Complete
     fun completeLastLogEntry() {
         logEntries.lastOrNull()?.let { lastLog ->
 
@@ -21,4 +24,53 @@ class LogViewModel : ViewModel() {
             logEntries[logEntries.lastIndex] = updatedLog
         }
     }
+
+
+    //=== Removes
+
+    // Remove 1 log
+//    fun removeSelectedLogEntry() {
+//        logEntries.removeLastOrNull()
+//    }
+
+    // Remove multiple selected logs
+//    fun removeSelectedLogEntries() {
+//        logEntries.clear()
+//    }
+
+    // Clear all logs
+//    fun clearAllLogs() {
+//        logEntries.clear()
+//    }
+
+    //=== Edits
+
+    // Edit log entry
+//    fun editLogEntry(index: Int, newLogEntry: LogEntry) {
+//        logEntries[index] = newLogEntry
+//    }
+
+    // Edit log entry category
+//    fun editLogEntryCategory(index: Int, newCategory: String) {
+//        logEntries[index] = logEntries[index].copy(logCategorization = newCategory)
+//    }
+
+    // Edit log entry start time
+//    fun editLogEntryStartTime(index: Int, newStartTime: Long) {
+//        logEntries[index] = logEntries[index].copy(startTimestamp = newStartTime)
+//    }
+
+    // Edit log entry end time
+//    fun editLogEntryEndTime(index: Int, newEndTime: Long) {
+//        logEntries[index] = logEntries[index].copy(endTimestamp = newEndTime)
+//    }
+
+    // Edit log entry activity
+//    fun editLogEntryActivity(index: Int, newActivity: String) {
+//        logEntries[index] = logEntries[index].copy(logActivity = newActivity)
+//    }
+
+
+
+
 }
