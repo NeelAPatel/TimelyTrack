3a6ff3274b640ac9b0df26632c784afb6fe3a22e
Neel <send2neelpatel@gmail.com>
2024-09-19 20:54:59 -0400

    test commit

--- app/src/main/java/com/example/timelytrack/MainActivity.kt
+++ app/src/main/java/com/example/timelytrack/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
 //class MainActivity : ComponentActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
